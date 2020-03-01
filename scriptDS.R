0c4d2017ef47542a38c30e692a6d59c726f90177
LARISSÃO <francyelid.lari@gmail.com>
2020-02-29 21:02:52 -0300

    Personalizando o gráfico

--- scriptDS.R
+++ scriptDS.R
@@ -27,3 +27,37 @@ x1 <- as.factor(x)
 
 # Fazendo um histograma dos dados
 hist(Airq$airq)
+hist(Airq$rain)
+
+
+hist(Airq$dens)
+hist(Airq$vala)
+plot(Airq$coas) # Para dados categóricos, é recomendado utilizar o plot, e não o hist
+
+# Gráficos Exploratórios
+
+# plot(y~x) # y corresponde à variável resposta, enquanto  x é a variável explicativa
+   # Sendo assim, y será airq, a qualidade do ar
+
+plot(Airq$airq ~ Airq$rain)
+# ou 
+plot(airq~rain, data = Airq)
+
+# Construindo retas
+abline(lm(Airq$airq ~ Airq$rain))
+
+plot(airq ~ dens, data = Airq)
+abline(lm(airq ~ dens, data = Airq))
+
+plot(airq ~ vala, data = Airq)
+abline(lm(Airq$airq ~ Airq$vala))
+
+# Personalizando os gráficos
+plot(airq ~ vala, data = Airq, xlab = "Valor das empresas", 
+     ylab = "Qualidade do ar", main = "Valor das empresas", 
+     sub = "Dados de 2017/2018", ylim = c(50, 200), xlim = c(500, 20000),
+     cex.lab = 1.3, cex = 1.3, pch = 16, col = "darkblue",
+     bty = "n")
+
+abline(lm(Airq$airq ~ Airq$vala), col = "darkred", lwd = 1.5)
+
