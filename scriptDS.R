adf1991a9972d20bc690c5fefe72bfb00318f49d
LARISSÃO <francyelid.lari@gmail.com>
2020-02-29 22:24:41 -0300

    Conclusão

--- scriptDS.R
+++ scriptDS.R
@@ -72,4 +72,33 @@ library(PerformanceAnalytics) # Esse pacote mostra o histograma de cada uma dess
 Airq2 <- Airq[,-5] # Para resolver isso, cria-se um banco de dados temporário com as variáveis que se busca um valor
 chart.Correlation(Airq2)
 
+# Modelo estatístico
+modelo <- glm(airq ~ vala + rain + coas + dens + medi, data = Airq)
 
+# Selecionando variáveis significativas
+library(MuMIn)
+options(na.action = "na.fail")
+
+dredge(modelo, rank = "AIC") # critério de akalke
+
+# Criando o gráfico final
+library(ggplot2)
+
+# Gráfico de regressão
+ggplot(data = Airq, aes(x = medi, y = airq)) + geom_point(size = 2) + 
+      geom_smooth(method = "glm", se = FALSE) + 
+      labs(x = "Renda média", y = "Qualidade do ar")
+
+# BoxPlot
+ggplot(data = Airq, aes(x = coas, y = airq)) + geom_boxplot(aes(color = coas)) + 
+       labs(x = "Posição costeira", y = "Qualidade do ar", color = "")
+
+# ANCOVA (y contínuo - x contínuo +  categórico)
+ggplot(data = Airq, aes(x = medi, y = airq, group = coas)) + geom_point(size = 2) + 
+      geom_smooth(method = "glm", aes(color = coas),se = FALSE) + 
+      labs(x = "Renda média", y = "Qualidade do ar", color = "Posição costeira")
+
+#################################################################################
+# CONCLUSÃO
+   # A qualidade do ar diminui confome a renda média aumenta. Além disso, cidades
+   # não costeira apresentam qualidade do ar pior quando comparado com cidades não costeiras.
